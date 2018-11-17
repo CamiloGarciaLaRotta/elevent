3e575c2b165dbc86ad96fadb4d0ffa7a67a42e54
Camilo Garcia La Rotta <camilo.e.garcia@mail.mcgill.ca>
2018-11-17 12:40:26 -0500

    Display results in DOM

--- backend/static/index.js
+++ backend/static/index.js
@@ -10,25 +10,45 @@ function main() {
       "In this class we will focus on improving your understanding of English Grammar and improving your vocabulary. The topics selected for teaching will be based on the needs of the students in the class",
       " Who we are: We are a group of people from a variety of different countries that meet once a week to chat, meet new people and practice other language(s)."
     ],
-    limit: 1
+    limit: 3
   };
 
-  (async () => {
-    const rawResponse = await fetch('http://localhost:5000/events', {
-      method: 'POST',
-      mode: "cors",
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-        "Access-Control-Allow-Origin" : "*",
-        "Access-Control-Allow-Credentials" : true
-      },
-      body: JSON.stringify(payload)
-    });
-    const content = await rawResponse.json()
+  post(payload).then(events => {
+    console.log(events);
+    displayEventsAsList(events);
+  })
+}
 
-    console.log(content);
-  })();
+async function post(payload) {
+  const rawResponse = await fetch('http://localhost:5000/events', {
+    method: 'POST',
+    mode: "cors",
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      "Access-Control-Allow-Origin" : "*",
+      "Access-Control-Allow-Credentials" : true
+    },
+    body: JSON.stringify(payload)
+  });
+  return await rawResponse.json()
+}
+
+function displayEventsAsList(events) {
+  let eventList = events.reduce((result, event) => {
+    result += `
+      <li>
+        <h3>
+          <a href="${event.url}">${event.name}</a>
+        </h3>
+        <ul>
+          <li>${event.group}</li>
+        </ul>
+      </li>`
+    return result;
+  }, '');
+
+  document.getElementById('main').innerHTML = `<ol>${eventList}</ol>`
 }
 
 (() => main())()
